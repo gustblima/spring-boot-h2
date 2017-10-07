bd96a36f7971668b9eccea7c641b4794021b5a84
= <gustavolima320@gmail.com>
2017-10-07 02:18:23 -0300

    Initialize Database

---
+++ src/main/java/com/gustavoblima/company/util/DataLoader.java
@@ -0,0 +1,61 @@
+package com.gustavoblima.company.util;
+
+import com.gustavoblima.company.converter.GenderConverter;
+import com.gustavoblima.company.dto.RUResultDTO;
+import com.gustavoblima.company.dto.RUWrapperDTO;
+import com.gustavoblima.company.entity.Company;
+import com.gustavoblima.company.entity.Employee;
+import com.gustavoblima.company.entity.Industry;
+import com.gustavoblima.company.repository.CompanyRepository;
+import com.gustavoblima.company.repository.EmployeeRepository;
+import com.gustavoblima.company.service.IEmployeeService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import javax.print.DocFlavor;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @Autowired
+    CompanyRepository companyRepository;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        RestTemplate restTemplate = new RestTemplate();
+        RUWrapperDTO randomUserResult = restTemplate
+                .getForObject("https://randomuser.me/api/?inc=gender,email,name,seed&results=10", RUWrapperDTO.class);
+
+        String seed = randomUserResult.getInfo().getSeed();
+        Random random = new Random();
+        String[] titles = new String[]{"Developer", "Assistant", "Engineer", "CEO"};
+        List<Company> companies = companyRepository.findAll();
+        randomUserResult.getResults().forEach((RUResultDTO user) -> {
+            Employee employee = new Employee();
+            employee.setName(user.getName().getFullName());
+            employee.setEmail(user.getEmail());
+            employee.setGender(new GenderConverter().convertToEntityAttribute(user.getGender()));
+            employee.setSeed(seed);
+            employee.setEmployer(companies.get(random.nextInt(companies.size())));
+            employee.setJobTitle(titles[random.nextInt(titles.length)]);
+            employee.setCpf("46262462");
+        });
+
+
+
+    }
+}
